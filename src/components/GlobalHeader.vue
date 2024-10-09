0fe1b339760e6d52592ba4c0636592dd5d0910b6
xhixxx <loongcyan@yeah.net>
2024-10-09 14:37:41 +0800

    first commit

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="titleBar">
+            <img class="logo" src="../assets/logo.png" />
+            <div class="title">鱼答答</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visbleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div v-if="loginUserStore.loginUser.id">
+        {{ loginUserStore.loginUser.userName ?? "匿名用户" }}
+      </div>
+      <div v-else></div>
+      <div>
+        <a-button type="primary" href="/user/login">登录</a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useLoginUserStore } from "@/store/userStore";
+
+const loginUserStore = useLoginUserStore();
+
+const router = useRouter();
+// 当前选择的菜单项
+const selectedKeys = ref(["/"]);
+// 路由跳转时，自动更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+// 展示在菜单栏的路由数组
+const visbleRoutes = routes.filter((item) => {
+  if (item.meta?.hideInMenu) {
+    return false;
+  }
+  return true;
+});
+
+// 点击菜单跳转到对应页面
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style scoped>
+#globalHeader {
+}
+
+.titleBar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="userLayout">
+    <a-layout style="height: 100vh">
+      <a-layout-header class="header">
+        <a-space>
+          <img class="logo" src="../assets/logo.png" />
+          <div>鱼答答 AI 答题应用平台</div>
+        </a-space>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view></router-view>
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/xhixxx" target="_blank">
+          github by 程序员戴夫
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#userLayout {
+  text-align: center;
+  background: url("https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png")
+    0% 0% / 100% 100%;
+}
+
+#userLayout .logo {
+  height: 48px;
+  width: 48px;
+}
+
+#userLayout .header {
+  margin-top: 16px;
+  font-size: 21px;
+}
+
+#userLayout .content {
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+.footer {
+  background: #efefef;
+  padding: 16px;
+  text-align: center;
+}
+</style>
